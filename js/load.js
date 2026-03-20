ad27432dda96614abf23253fbbcdca831e075f53
miltonossandon <sazoossandonmilton@gmail.com>
2026-03-19 23:27:29 -0300

    Initial commit

---
+++ js/load.js
@@ -0,0 +1,93 @@
+fetch("components/header.html")
+.then(res => res.text())
+.then(data => {
+document.getElementById("header").innerHTML = data;
+});
+
+fetch("components/footer.html")
+.then(res => res.text())
+.then(data => {
+document.getElementById("footer").innerHTML = data;
+});
+
+
+document.addEventListener("DOMContentLoaded", function(){
+
+const track = document.querySelector(".carousel-track");
+if(!track) return;
+
+const slides = track.querySelectorAll(".bloque");
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+const dotsContainer = document.querySelector(".carousel-dots");
+
+let index = 0;
+
+
+/* CREAR PUNTOS */
+
+slides.forEach((_, i) => {
+
+const dot = document.createElement("div");
+dot.classList.add("dot");
+
+if(i === 0){
+dot.classList.add("active");
+}
+
+dot.addEventListener("click", () => {
+index = i;
+updateCarousel();
+});
+
+dotsContainer.appendChild(dot);
+
+});
+
+const dots = dotsContainer.querySelectorAll(".dot");
+
+
+/* ACTUALIZAR CARRUSEL */
+
+function updateCarousel(){
+
+track.style.transform = `translateX(-${index * 100}%)`;
+
+dots.forEach(dot => dot.classList.remove("active"));
+dots[index].classList.add("active");
+
+}
+
+
+/* BOTON SIGUIENTE */
+
+next.addEventListener("click", () => {
+
+index++;
+
+if(index >= slides.length){
+index = 0;
+}
+
+updateCarousel();
+
+});
+
+
+/* BOTON ANTERIOR */
+
+prev.addEventListener("click", () => {
+
+index--;
+
+if(index < 0){
+index = slides.length - 1;
+}
+
+updateCarousel();
+
+});
+
+});
+
+
