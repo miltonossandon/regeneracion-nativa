8b1b10453254c4008f1335a6a613b96efaa436a7
miltonossandon <sazoossandonmilton@gmail.com>
2026-03-21 15:05:23 -0300

    Actualización a movil
    Se actualiza la pagina web a telefonos movil en su version 1.0

--- js/load.js
+++ js/load.js
@@ -1,9 +1,23 @@
+// ===== CARGA HEADER =====
 fetch("components/header.html")
 .then(res => res.text())
 .then(data => {
 document.getElementById("header").innerHTML = data;
+
+/* ACTIVAR MENU HAMBURGUESA DESPUÉS DE CARGAR */
+const toggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector("nav");
+
+if(toggle && nav){
+toggle.addEventListener("click", () => {
+nav.classList.toggle("active");
+});
+}
+
 });
 
+
+// ===== CARGA FOOTER =====
 fetch("components/footer.html")
 .then(res => res.text())
 .then(data => {
@@ -11,6 +25,7 @@ document.getElementById("footer").innerHTML = data;
 });
 
 
+// ===== CARRUSEL =====
 document.addEventListener("DOMContentLoaded", function(){
 
 const track = document.querySelector(".carousel-track");
@@ -25,7 +40,6 @@ let index = 0;
 
 
 /* CREAR PUNTOS */
-
 slides.forEach((_, i) => {
 
 const dot = document.createElement("div");
@@ -48,7 +62,6 @@ const dots = dotsContainer.querySelectorAll(".dot");
 
 
 /* ACTUALIZAR CARRUSEL */
-
 function updateCarousel(){
 
 track.style.transform = `translateX(-${index * 100}%)`;
@@ -60,7 +73,6 @@ dots[index].classList.add("active");
 
 
 /* BOTON SIGUIENTE */
-
 next.addEventListener("click", () => {
 
 index++;
@@ -75,7 +87,6 @@ updateCarousel();
 
 
 /* BOTON ANTERIOR */
-
 prev.addEventListener("click", () => {
 
 index--;
@@ -89,5 +100,3 @@ updateCarousel();
 });
 
 });
-
-
